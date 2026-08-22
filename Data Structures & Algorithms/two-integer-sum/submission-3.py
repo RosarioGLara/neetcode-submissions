2d5801f4f81ac0c4000993eacf3eb7fcd9b056fa
Rosario G Lara <rglara2@miners.utep.edu>
2026-08-21 19:02:05 -0600

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        tracker = {}
+
+        for i in range(len(nums)):
+            res = target - nums[i]
+            if nums[i] in tracker.keys():
+                return [tracker[nums[i]], i]
+            tracker[res] = i
+        
+        return -1
